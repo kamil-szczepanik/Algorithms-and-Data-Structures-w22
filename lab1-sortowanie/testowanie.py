df2adb248b61a4d3e0ce31a79140ba556d7b3fde
kszczep4 <01149444@pw.edu.pl>
2022-11-15 17:26:44 +0100

    dodanie sprawozdania

--- lab1-sortowanie/testowanie.py
+++ lab1-sortowanie/testowanie.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 sys.setrecursionlimit(10000)
-N = [500, 1000, 2000,5000, 10000]
+N = [100, 500, 1000, 2000, 3000, 5000, 10000, 15000]
 Wyniki = []
 import gc
 gc_old = gc.isenabled() # pobierz aktualny stan odśmiecania
@@ -37,32 +37,43 @@ plt.plot(N,Wyniki[1],label='Selection')
 plt.plot(N,Wyniki[2],label='Merge')
 plt.plot(N,Wyniki[3],label='Quick')
 plt.legend()
+plt.xlabel("Długość listy")
+plt.ylabel("Czas sortowania")
 # plt.show()
 plt.savefig("wykres1.png")
 plt.close()
 
 f = plt.figure()
-f.set_figwidth(10)
-f.set_figheight(8)
+f.set_figwidth(16)
+f.set_figheight(12)
+f.tight_layout(pad=100.0)
 
 plt.subplot(221)
-plt.plot(N,Wyniki[0])
+plt.plot(N,Wyniki[0], color="blue")
 plt.title('Bubble')
+plt.xlabel("Długość listy")
+plt.ylabel("Czas sortowania")
 plt.grid(True)
 
 plt.subplot(222)
-plt.plot(N,Wyniki[1])
+plt.plot(N,Wyniki[1], color="orange")
 plt.title('Selection')
+plt.xlabel("Długość listy")
+plt.ylabel("Czas sortowania")
 plt.grid(True)
 
 plt.subplot(223)
-plt.plot(N,Wyniki[2])
+plt.plot(N,Wyniki[2], color="green")
 plt.title('Merge')
+plt.xlabel("Długość listy")
+plt.ylabel("Czas sortowania")
 plt.grid(True)
 
 plt.subplot(224)
-plt.plot(N,Wyniki[3])
+plt.plot(N,Wyniki[3], color="red")
 plt.title('Quick')
+plt.xlabel("Długość listy")
+plt.ylabel("Czas sortowania")
 plt.grid(True)
 # plt.show()
 plt.savefig("wykres2.png")
