b58380010c5910f516bb6d9dbae5559bf77d0d02
kszczep4 <01149444@pw.edu.pl>
2022-11-27 00:21:23 +0100

    dodanie search i delete do BST oraz calego AVL

--- lab2-drzewa/BinaryTree.py
+++ lab2-drzewa/BinaryTree.py
@@ -2,38 +2,262 @@ import random
 
 
 class Node:
-  def __init__(self,data):
-    self.left = None
-    self.right = None
-    self.data = data
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+        self.height = 1
 
-  def insert(self,data):
-    if self.data:
-      if data < self.data:
-        if self.left is None:
-          self.left = Node(data)
-        else:
-          self.left.insert(data)
-      elif data > self.data:
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.data
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
         if self.right is None:
-          self.right = Node(data)
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.data
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.data
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.data
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.data, end=" < ")
+        if self.right:
+            self.right.PrintTree()
+
+
+class BST():
+    def __init__(self):
+        self.root = None
+    
+    def insert(self,data):
+        self.root = self._insert(self.root, data)
+
+    def _insert(self, root, data):
+        if not root:
+            return Node(data)
+        elif data < root.data:
+            root.left = self._insert(root.left, data)
         else:
-          self.right.insert(data)
-    else:
-      self.data = data
+            root.right = self._insert(root.right, data)
+
+        root.height = 1 + max(self.get_height(root.left),
+                        self.get_height(root.right))
+        return root
+
+    def find_most_left(self, root):
+        if root is None or root.left is None:
+            return root
+        return self.find_most_left(root.left)
+
+    def search(self, data):
+        return self._search(self.root, data)
+    
+    def _search(self, root, data):
+        if not root:
+            return root
+        if root.data == data:
+            return root
+        if data < root.data:
+            return self._search(root.left, data)
+        if data > root.data:
+            return self._search(root.right, data)
+
+    def delete(self, data):
+        self.root = self._delete(self.root, data)
+
+    def _delete(self, root, data):
+        if not root:
+            return root
+        if data < root.data:
+            root.left = self._delete(root.left, data)
+        elif data > root.data:
+            root.right = self._delete(root.right, data)
+        else:
+            if root.left is None:
+                root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))     
+                return root.right
+            if root.right is None:
+                root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))  
+                return root.left
+            
+            most_left_root = self.find_most_left(root.right)
+            root.data = most_left_root.data
+            root.right = self._delete(root.right, root.data)   
+
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))         
+        
+        return root
+
+    def get_height(self, root):
+        if not root:
+            return 0
+        return root.height
 
 
-  def PrintTree(self):
-    if self.left:
-      self.left.PrintTree()
-    print(self.data, end=" < ")
-    if self.right:
-      self.right.PrintTree()
+class AVL(BST):
+    def __init__(self):
+        super().__init__()
+
+    def get_balance(self, root):
+        if not root:
+            return 0
+        return self.get_height(root.left) - self.get_height(root.right)
+
+    def display(self):
+        self.root.display()
+
+    def insert(self,data):
+        self.root = self._insert(self.root, data)
+
+    def _insert(self, root, data):
+        root = super()._insert(root, data)
+
+        balance = self.get_balance(root)
+    
+        if balance > 1 and data < root.left.data:
+            return self.right_rotation(root)
+
+        if balance < -1 and data > root.right.data:
+            return self.left_rotation(root)
+
+        if balance > 1 and data > root.left.data:
+            root.left = self.left_rotation(root.left)
+            return self.right_rotation(root)
+
+        if balance < -1 and data < root.right.data:
+            root.right = self.right_rotation(root.right)
+            return self.left_rotation(root)
+
+        return root
+
+    def delete(self, data):
+        self.root = self._delete(self.root, data)
+
+    def _delete(self, root, data):
+        root = super()._delete(root, data)
+
+
+        balance = self.get_balance(root)
+
+        if balance > 1:
+            if self.get_balance(root.left) >= 0:
+                return self.right_rotation(root)
+            else:
+                root.left = self.left_rotation(root.left)
+                return self.right_rotation(root)
+
+        if balance < -1:
+            if self.get_balance(root.right) <= 0:
+                return self.left_rotation(root)
+            else:
+                root.right = self.right_rotation(root.right)
+                return self.left_rotation(root)
+
+        return root
+
+ 
+    def left_rotation(self, z):
+ 
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        z.height = 1 + max(self.get_height(z.left),
+                         self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left),
+                         self.get_height(y.right))
+
+        return y
+ 
+    def right_rotation(self, z):
+ 
+        y = z.left
+        T3 = y.right
+
+        y.right = z
+        z.left = T3
+
+        z.height = 1 + max(self.get_height(z.left),
+                        self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left),
+                        self.get_height(y.right))
+
+        return y
+
 
 if __name__ == '__main__':
-  root = Node(5000)
-  for i in range(100):
-  
-    intTemp = random.randint(1,10000)
-    root.insert(intTemp)
-  root.PrintTree()
+    bst_tree = BST()
+    avl_tree = AVL()
+    for i in range(10):
+        intTemp = random.randint(1,10000)
+        if i == 3:
+            x = intTemp
+        bst_tree.insert(intTemp)
+        avl_tree.insert(intTemp)
+
+
+    print("BST: \n")
+    bst_tree.root.display()
+    print('to be deleted: ', x)
+    print("Searching: ", x, " - result: ", bool(bst_tree.search(x)))
+    print("Searching: ", 1111, " - result: ", bool(bst_tree.search(1111)))
+    bst_tree.delete(x)
+    print('height: ', bst_tree.get_height(bst_tree.root))
+    bst_tree.root.display()
+
+    print("==================\nAVL: \n")
+    avl_tree.root.display()
+    print("Searching: ", x, " - result: ", bool(avl_tree.search(x)))
+    print("Searching: ", 1111, " - result: ", bool(avl_tree.search(1111)))
+    print('to be deleted: ', x)
+    avl_tree.delete(x)
+    print('height: ', avl_tree.get_height(avl_tree.root))
+    print('balance: ', avl_tree.get_balance(avl_tree.root))
+    avl_tree.root.display()
+
+
+    avl_tree.root.PrintTree()
